acc1b68855f96de81ee2ec04d4b6f5b6ac2fbc74
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-07-03 18:15:15 -0700

    Did ther servo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,15 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.TeleopArcadeDriveCmd;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.xrp.XRPServo;
+import frc.robot.subsystems.Arm;
 
 public class RobotContainer {
   private final CommandXboxController m_XboxController = new CommandXboxController(0);
   private final Drivetrain m_drivetrain = new Drivetrain(0, 1);
   private final TeleopArcadeDriveCmd m_TeleopArcadeDriveCmd = new TeleopArcadeDriveCmd(m_drivetrain, m_XboxController::getLeftY, m_XboxController::getRightY);
-
+  private final XRPServo m_servo = new XRPServo(4);
+  private final Arm m_arm = new Arm(m_servo);
 
 
   public RobotContainer() {
@@ -23,11 +26,24 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    m_drivetrain.setDefaultCommand(m_TeleopArcadeDriveCmd);
-  }
+   // m_drivetrain.setDefaultCommand(m_TeleopArcadeDriveCmd); 
+
+  //  m_drivetrain.setDefaultCommand(getArcadeDriveCommand());
+  m_drivetrain.setDefaultCommand(new RunCommand(() -> { m_drivetrain.arcadeDrive(-m_XboxController.getLeftY(), m_XboxController.getRightX());}, m_drivetrain));
+  m_arm.setDefaultCommand(moveArm(0));
+  m_XboxController.a().whileTrue(moveArm(45));
+  m_XboxController.b().whileTrue(moveArm(90));
+  m_XboxController.x().whileTrue(moveArm(135));
+  m_XboxController.y().whileTrue(moveArm(180));
+  
+  } 
 
   private Command getArcadeDriveCommand() {
     return new RunCommand(() -> { m_drivetrain.arcadeDrive(-m_XboxController.getLeftY(), m_XboxController.getRightX());}, m_drivetrain);
+  }
+
+  private Command moveArm(int angle) {
+    return new RunCommand(() -> m_arm.setAngle(angle));
   }
 
   public Command getAutonomousCommand() {
